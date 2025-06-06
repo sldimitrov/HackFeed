11f2d2ba9734e8f8e5f8a7144c3d45f49c7c3eb8
Slavi FE <slavidimitrov54@gmail.com>
2025-06-06 14:46:16 +0300

    feat(profiles) create services, queries and types for the user profile

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,27 @@
+// hooks/useUserProfile.ts
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { QUERY_PROFILE } from '../contants/queryKeys.ts';
+import UserService from '../services/userService.ts';
+import type { UserProfile } from '../types/profile.ts';
+
+export function useUserProfile(userId: string | undefined) {
+  return useQuery({
+    queryKey: [QUERY_PROFILE, userId],
+    queryFn: () => {
+      if (!userId) throw new Error('Missing user ID');
+      return UserService.getProfile(userId);
+    },
+    enabled: !!userId, // не се изпълнява без userId
+  });
+}
+
+export function useUpdateUserProfile(userId: string) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (updates: Partial<UserProfile>) => UserService.updateProfile(userId, updates),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_PROFILE, userId] });
+    },
+  });
+}
